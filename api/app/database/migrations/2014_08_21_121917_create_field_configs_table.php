e88241870be28dbb2371f160bdcf87e679d091ca
Kliment <kliment.lambevski@gmail.com>
2014-08-21 14:19:46 +0200

    popravka vo migracii

--- api/app/database/migrations/2014_08_21_121917_create_field_configs_table.php
+++ api/app/database/migrations/2014_08_21_121917_create_field_configs_table.php
@@ -5,33 +5,33 @@ use Illuminate\Database\Schema\Blueprint;
 
 class CreateFieldConfigsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('field_configs', function(Blueprint $table)
-		{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('field_configs', function(Blueprint $table)
+        {
             $table->increments('id');
             $table->integer('field_id')->unsigned()->index();
             $table->foreign('field_id')->references('id')->on('fields')->onDelete('cascade');
             $table->string('key');
             $table->string('value');
             $table->timestamps();
-		});
-	}
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('field_configs');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('field_configs');
+    }
 
 }
